adeedf072f1f127b0fc5e5ee832668efe7351555
Amr Abdelmeguid <medialabadmin@NB3697922.students.hsrw>
2026-01-09 02:54:38 +0100

    Publish Angular site to gh-pages

--- apps/web/src/envirnoments/envirnoment.prod.ts
+++ apps/web/src/envirnoments/envirnoment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://bpdkltgikgbnfjswdbaj.supabase.co',
   supabaseAnonKey: 'sb_publishable_17VL78hCv9BVez2c4lIqFQ_MUv1iOHb',
-  graphqlEndpoint: 'https://world-app-skeleton.onrender.com/graphql',
+  graphqlEndpoint: 'https://api.matterya.com/graphql',
 };
